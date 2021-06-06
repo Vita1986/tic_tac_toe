e882d007c13fb59471763f052c13d229e09f9748
Name <email@example.ru>
2021-06-06 12:52:14 +0300

    tic_tac_toe
    tic_tac_toe

--- main.py
+++ main.py
@@ -1,86 +1,83 @@
-def greet():
-    print("-------------------")
-    print("  Приветсвуем вас  ")
-    print("      в игре       ")
-    print("  крестики-нолики  ")
-    print("-------------------")
-    print(" формат ввода: x y ")
-    print(" x - номер строки  ")
-    print(" y - номер столбца ")
+import pygame
+import random
 
-def show():
-    print()
-    print("    | 0 | 1 | 2 | ")
-    print("  --------------- ")
-    for i, row in enumerate(field):
-        row_str = f"  {i} | {' | '.join(row)} | "
-        print(row_str)
-        print("  --------------- ")
-    print()
+pygame.init()
 
-def ask():
-    while True:
-        cords = input("         Ваш ход: ").split()
-        
-        if len(cords) != 2:
-            print(" Введите 2 координаты! ")
-            continue
-        
-        x, y = cords
-        
-        if not(x.isdigit()) or not(y.isdigit()):
-            print(" Введите числа! ")
-            continue
-        
-        x, y = int(x), int(y)
-        
-        if 0 > x or x > 2 or  0 > y or  y > 2 :
-            print(" Координаты вне диапазона! ")
-            continue
-        
-        if field[x][y] != " ":
-            print(" Клетка занята! ")
-            continue
-        
-        return x, y
-            
-def check_win():
-    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
-                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
-                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
-    for cord in win_cord:
-        symbols = []
-        for c in cord:
-            symbols.append(field[c[0]][c[1]])
-        if symbols == ["X", "X", "X"]:
-            print("Выиграл X!!!")
-            return True
-        if symbols == ["0", "0", "0"]:
-            print("Выиграл 0!!!")
-            return True
-    return False
 
-greet()
-field = [[" "] * 3 for i in range(3) ]
-count = 0
-while True:
-    count += 1
-    show()
-    if count % 2 == 1:
-        print(" Ходит крестик!")
-    else:
-        print(" Ходит нолик!")
-    
-    x, y = ask()
-    
-    if count % 2 == 1:
-        field[x][y] = "X"
-    else:
-        field[x][y] = "0"
-    
-    if check_win():
-        break
-    
-    if count == 9:
-        print(" Ничья!")
-        break
+def draw_grid(scr):
+    pygame.draw.line(scr, (0, 0, 0), (100, 0), (100, 300), 3)
+    pygame.draw.line(scr, (0, 0, 0), (200, 0), (200, 300), 3)
+    pygame.draw.line(scr, (0, 0, 0), (0, 100), (300, 100), 3)
+    pygame.draw.line(scr, (0, 0, 0), (0, 200), (300, 200), 3)
+
+
+def draw_tic_tac_toe(scr, items):
+    for i in range(3):
+        for j in range(3):
+            if items[i][j] == "0":
+                pygame.draw.circle(scr, (255, 0, 0), (j * 100 + 50, i * 100 + 50), 45)
+            elif items[i][j] == "x":
+                pygame.draw.line(scr, (0, 0, 255), (j * 100 + 5, i * 100 + 5), (j * 100 + 95, i * 100 + 95), 3)
+                pygame.draw.line(scr, (0, 0, 255), (j * 100 + 95, i * 100 + 5), (j * 100 + 5, i * 100 + 95), 3)
+
+
+def get_win_check(fd, symbol):
+    flag_win = False
+    for line in fd:
+        if line.count(symbol) == 3:
+            flag_win = True
+    for i in range(3):
+        if fd[0][i] == fd[1][i] == fd[2][i] == symbol:
+            flag_win = True
+    if fd[0][0] == fd[1][1] == fd[2][2] == symbol:
+        flag_win = True
+    if fd[0][2] == fd[1][1] == fd[2][0] == symbol:
+        flag_win = True
+    return flag_win
+
+
+SCREEN_SIZE = (300, 300)
+
+window = pygame.display.set_mode(SCREEN_SIZE)
+screen = pygame.Surface(SCREEN_SIZE)
+
+pygame.display.set_caption("Крестики-нолики")
+screen.fill((255, 255, 255))
+
+field = [["", "", ""],
+         ["", "", ""],
+         ["", "", ""]]
+mainloop = True
+game_over = False
+
+while mainloop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mainloop = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            pos = pygame.mouse.get_pos()
+            if field[pos[1] // 100][pos[0] // 100] == "":
+                field[pos[1] // 100][pos[0] // 100] = "x"
+                x, y = random.randint(0, 2), random.randint(0, 2)
+                while field[x][y] != "":
+                    x, y = random.randint(0, 2), random.randint(0, 2)
+                field[x][y] = "0"
+
+            player_win = get_win_check(field, "x")
+            ai_win = get_win_check(field, "0")
+            if player_win or ai_win:
+                game_over = True
+                if player_win:
+                    pygame.display.set_caption("Вы победили")
+                else:
+                    pygame.display.set_caption("Компьютер победил")
+            elif field[0].count("x") + field[0].count("0") + field[1].count("x") + \
+                    field[1].count("0") + field[2].count("x") + field[2].count("0") == 8:
+                game_over = True
+                pygame.display.set_caption("Ничья")
+
+    draw_tic_tac_toe(screen, field)
+    draw_grid(screen)
+    window.blit(screen, (0, 0))
+    pygame.display.update()
